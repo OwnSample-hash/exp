93d39cc05a6bad555ca663dd1d320cf7b3e43cc8
OwnSample <ownsample@tutanota.com>
2025-12-25 12:26:19 +0100

    feat: delete example module

--- modules/example/src/example.cpp
+++
@@ -1,2 +0,0 @@
-#include <spdlog/spdlog.h>
-void module_entry() { spdlog::info("Example module (C++20) loaded"); }
--- modules/example/test/test_example.cpp
+++
@@ -1,5 +0,0 @@
-#include "../include/example.hpp"
-#include <catch2/catch_test_macros.hpp>
-
-TEST_CASE("Example module loads") { REQUIRE_NOTHROW(module_entry()); }
-// Vim: set expandtab tabstop=2 shiftwidth=2:
