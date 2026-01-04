4770c5b6dc65188f37f376aa99c57b1dd8f7f521
OwnSample <ownsample@tutanota.com>
2026-01-04 17:37:09 +0100

    feat: placeholder dynamic module loading

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <modules.hpp>
 
-int main() {
-  std::cout << "Platform: " << platform_name << "\n";
-  module_entry_test();
-}
+void load_module_dynamic(const char *name) {}
+
+int main() { std::cout << "Platform: " << platform_name << "\n"; }
 // Vim: set expandtab tabstop=2 shiftwidth=2:
