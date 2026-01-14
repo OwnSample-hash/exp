81cb416e2fce8e4fd3a986e416f2ddae58d9ae14
OwnSample <ownsample@tutanota.com>
2026-01-14 18:13:12 +0100

    feat: basic static module loading

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,7 +1,31 @@
+#include <config.h>
 #include <iostream>
+#include <iterator>
+#include <module.hpp>
 #include <modules.hpp>
+#include <spdlog/spdlog.h>
+#include <vector>
+
+#error fix app/CMakeLists.txt to link new_module correctly
+
+explo::Module static_modules[] = {
+    explo::Module{0, "core",
+                  explo::MODULE_TYPE_CORE | explo::MODULE_TYPE_STATIC,
+                  std::make_unique<new_module>()},
+
+};
+
+std::vector<explo::Module> modules{
+    std::make_move_iterator(std::begin(static_modules)),
+    std::make_move_iterator(std::end(static_modules)),
+};
 
 void load_module_dynamic(const char *name) {}
 
-int main() { std::cout << "Platform: " << platform_name << "\n"; }
+int main() {
+  spdlog::info("Platform: {}", platform_name);
+  for (const auto &mod : modules) {
+    spdlog::info("Loaded module: {}", mod.name);
+  }
+}
 // Vim: set expandtab tabstop=2 shiftwidth=2:
