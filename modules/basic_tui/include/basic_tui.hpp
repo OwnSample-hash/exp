2641cea7c8faed23b962f5c58c23e60759fbf5a6
OwnSample <ownsample@tutanota.com>
2026-01-24 18:59:07 +0100

    feat: basic printf style tui

---
+++ modules/basic_tui/include/basic_tui.hpp
@@ -0,0 +1,44 @@
+#pragma once
+#include <color.hpp>
+#include <functional>
+#include <interfaces/display.hpp>
+#include <list>
+#include <module.hpp>
+#include <plugin_interface.hpp>
+#include <string_view>
+
+using namespace explo;
+
+class PL_basic_tui : public IPlugin {
+public:
+  PL_basic_tui() = default;
+  ~PL_basic_tui() = default;
+  std::string_view getName() const;
+  std::string_view getVersion() const;
+  void initialize(std::list<Module> &modules);
+  void execute();
+};
+
+class basic_tui : public IDisplay {
+public:
+  basic_tui();
+  ~basic_tui() override;
+
+  const char *getName() const override;
+  const char *getVersion() const override;
+
+  void initialize() override;
+  void shutdown() override;
+
+  void clearScreen() override;
+  void updateScreen() override;
+
+  int getWidth() const override;
+  int getHeight() const override;
+
+  void drawText(int x, int y, std::string_view text, Color fg = Color::WHITE,
+                Color bg = Color::BLACK) override;
+
+  void onInputChar(std::function<void(char)>) override;
+};
+// Vim: set expandtab tabstop=2 shiftwidth=2:
---
+++ modules/basic_tui/src/basic_tui.cpp
@@ -0,0 +1,71 @@
+#include <basic_tui.hpp>
+
+std::string_view PL_basic_tui::getName() const {
+  return "basic_tui";
+}
+
+std::string_view PL_basic_tui::getVersion() const {
+  return "0.0.1";
+}
+
+void PL_basic_tui::initialize(std::list<explo::Module> &modules) {
+  // Register the display module
+  modules.emplace_back("basic display module", explo::MODULE_TYPE_DISPLAY, std::make_unique<basic_tui>());
+}
+
+void PL_basic_tui::execute() {
+  // Plugin execution implementation
+}
+
+static PluginRegistry::Add<PL_basic_tui> basic_tuiRegister(basic_tui);
+
+// Belongs to the module type(s): basic_tui
+
+const char *basic_tui::getName() const {
+  return "basic_tui";
+}
+const char *basic_tui::getVersion() const {
+  return "0.1.0";
+}
+
+void basic_tui::initialize() {
+  // Module initialization implementation
+}
+void basic_tui::shutdown() {
+  // Module shutdown implementation
+}
+
+basic_tui::basic_tui() {
+  // Constructor implementation
+}
+
+basic_tui::~basic_tui() {
+  // Deconstructor implementation
+}
+
+void basic_tui::clearScreen() {
+  // Implementation to clear the screen
+}
+
+void basic_tui::updateScreen() {
+  // Implementation to update the screen
+}
+
+int basic_tui::getWidth() const {
+  // Implementation to get the width of the display
+  return 0;
+}
+
+int basic_tui::getHeight() const {
+  // Implementation to get the height of the display
+  return 0;
+}
+
+void basic_tui::drawText(int x, int y, std::string_view text, Color fg, Color bg) {
+  // Implementation to draw text on the display
+}
+
+void basic_tui::onInputChar(std::function<void(char)> callback) {
+  // Implementation to handle input characters
+}
+// Vim: set expandtab tabstop=2 shiftwidth=2:
