f2c089a85861b5246c11120d35fd5ff24a61daf6
OwnSample <ownsample@tutanota.com>
2026-01-24 18:47:14 +0100

    delete: platform, will be moved to the main app

--- modules/platform/src/platform.cpp
+++
@@ -1,9 +0,0 @@
-#if defined(_WIN32)
-const char *platform_name = "Windows";
-#elif defined(__APPLE__)
-const char *platform_name = "macOS";
-#else
-const char *platform_name = "Linux/Unix";
-#endif
-
-// Vim: set expandtab tabstop=2 shiftwidth=2:
