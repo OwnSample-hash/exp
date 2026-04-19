c8e4642309cb24e1d8c893de8e0f165c9f8f4115
OwnSample <ownsample@tutanota.com>
2026-04-19 09:24:15 +0200

    chore: update cpm packge lock

--- cpm-package-lock.cmake
+++ cpm-package-lock.cmake
@@ -3,15 +3,15 @@
 
 # spdlog
 CPMDeclarePackage(spdlog
-  VERSION 1.16.0
+  VERSION 1.17.0
   GITHUB_REPOSITORY gabime/spdlog
   SYSTEM YES
   EXCLUDE_FROM_ALL YES
 )
-# Catch2
-CPMDeclarePackage(Catch2
-  VERSION 3.11.0
-  GITHUB_REPOSITORY catchorg/Catch2
-  SYSTEM YES
-  EXCLUDE_FROM_ALL YES
+# lua
+CPMDeclarePackage(lua
+  NAME lua
+  VERSION 5.5.0
+  DOWNLOAD_ONLY YES
+  GIT_REPOSITORY https://github.com/lua/lua.git
 )
