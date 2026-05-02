143947a3c3437ad4aa8edece88f13182fcc763c3
OwnSample <ownsample@tutanota.com>
2026-05-02 16:46:31 +0200

    feat: var checks if empty and return nil, basic call function to call another tool

--- plugins/lua_exp/src/lib.cpp
+++ plugins/lua_exp/src/lib.cpp
@@ -1,4 +1,5 @@
 #include <cmd.hpp>
+#include <cmd/variable.hpp>
 #include <lib.hpp>
 #include <spdlog/spdlog.h>
 #include <string>
@@ -23,11 +24,46 @@ int log(lua_State *L) {
 int var(lua_State *L) {
   const char *env_var = luaL_checkstring(L, 1);
   auto var = explo::cmd::CommandProcessor::instance().vars().get(env_var);
-  if (var) {
-    lua_pushstring(L, var->toString().c_str());
-  } else {
+  if (!var) {
     lua_pushnil(L);
+    return 1;
   }
+  switch (var->type) {
+  case explo::cmd::VarType::String:
+    lua_pushstring(L, var->toString().c_str());
+  case explo::cmd::VarType::Integer:
+    lua_pushinteger(L, var->toInt());
+    break;
+  case explo::cmd::VarType::Float:
+    lua_pushnumber(L, var->fval);
+    break;
+  case explo::cmd::VarType::Bool:
+    lua_pushboolean(L, var->toBool());
+    break;
+  case explo::cmd::VarType::Array: {
+    static auto logger = spdlog::get("lua_exp")->clone("lua_exp::log");
+    logger->warn("Lua attempted to access array variable '{}', which is not "
+                 "directly supported. Returning nil.",
+                 env_var);
+    lua_pushnil(L);
+    break;
+  }
+  }
+  return 1;
+}
+
+int call(lua_State *L) {
+  const char *cmd_name = luaL_checkstring(L, 1);
+  std::vector<std::string> args;
+  int nargs = lua_gettop(L);
+  for (int i = 2; i <= nargs; i++) {
+    if (lua_isstring(L, i)) {
+      args.push_back(lua_tostring(L, i));
+    } else {
+      args.push_back("<non-string argument>");
+    }
+  }
+  lua_pushstring(L, "<s>");
   return 1;
 }
 
