cfa6428e51b8526c7ec876ec501729031dee27b8
OwnSample <ownsample@tutanota.com>
2026-05-02 16:43:22 +0200

    feat: rewrite the declare part to use x macros

--- plugins/lua_exp/include/lib.hpp
+++ plugins/lua_exp/include/lib.hpp
@@ -5,13 +5,26 @@ extern "C" {
 #include <lua.h>
 #include <lualib.h>
 }
+#include <interfaces/tool.hpp>
+#include <map>
+#include <memory>
+#include <string>
 
-int log(lua_State *L);
+extern std::map<std::string, std::shared_ptr<explo::ITool>> tools;
 
-int var(lua_State *L);
+#define luaFuncs                                                               \
+  X(log)                                                                       \
+  X(var)                                                                       \
+  X(call)
 
-const luaL_Reg libs[] = {
-    {"log", log},
-    {"var", var},
-    {nullptr, nullptr},
+#define X(name) int name(lua_State *L);
+luaFuncs
+#undef X
+
+    const luaL_Reg libs[] = {
+#define X(name) {#name, name},
+        luaFuncs
+#undef X
+        {nullptr, nullptr},
 };
+// Vim: set expandtab tabstop=2 shiftwidth=2:
