bc3ee427a7471880586705873a46dde151fa9b9d
OwnSample <ownsample@tutanota.com>
2026-06-03 22:46:30 +0200

    feat: updated templates to use new renderer stuff

--- templates/include/template_Display.hpp
+++ templates/include/template_Display.hpp
@@ -1,11 +1,8 @@
 // clang-format off
 #pragma once
-#include <functional>
-#include <interfaces/display.hpp>
-#include <list>
+#include <interfaces/renderer.hpp>
 #include <module.hpp>
 #include <plugin_interface.hpp>
-#include <string_view>
 
 using namespace explo;
 
@@ -13,13 +10,12 @@ class PL_%s final : public IPlugin {
 public:
   PL_%s() = default;
   ~PL_%s() = default;
-  std::string_view getName() const override;
-  std::string_view getVersion() const override;
+  const char* getName() const override;
+  const char* getVersion() const override;
   void initialize(initArgs &args) override;
-  void execute() override;
 };
 
-class %s final : public IDisplay {
+class %s final : public IRenderer {
 public:
   %s();
   ~%s() override;
@@ -30,15 +26,7 @@ public:
   void initialize() override;
   void shutdown() override;
 
-  void clearScreen() override;
-  void updateScreen() override;
+  void runLoop() override;
 
-  int getWidth() const override;
-  int getHeight() const override;
-
-  void drawText(int x, int y, std::string_view text, Color fg = Color::WHITE,
-                Color bg = Color::BLACK) override;
-
-  void onInputChar(std::function<void(char)>) override;
 };
 // Vim: set expandtab tabstop=2 shiftwidth=2:
--- templates/src/template_Display.cpp
+++ templates/src/template_Display.cpp
@@ -1,21 +1,17 @@
 // clang-format off
 #include <%s.hpp>
 
-std::string_view PL_%s::getName() const {
+const char* PL_%s::getName() const {
   return "%s";
 }
 
-std::string_view PL_%s::getVersion() const {
+const char* PL_%s::getVersion() const {
   return %s;
 }
 
 void PL_%s::initialize(initArgs &args) {
   // Register the display module
-  modules.emplace_back(%s, explo::MODULE_TYPE_DISPLAY, std::make_unique<%s>());
-}
-
-void PL_%s::execute() {
-  // Plugin execution implementation
+  args.modules->emplace_back(%s, explo::ModuleType::RENDERER, std::make_unique<%s>());
 }
 
 static PluginRegistry::Add<PL_%s> %sRegister("%s");
@@ -44,29 +40,8 @@ void %s::shutdown() {
   // Deconstructor implementation
 }
 
-void %s::clearScreen() {
+void %s::runLoop() {
   // Implementation to clear the screen
 }
 
-void %s::updateScreen() {
-  // Implementation to update the screen
-}
-
-int %s::getWidth() const {
-  // Implementation to get the width of the display
-  return 0;
-}
-
-int %s::getHeight() const {
-  // Implementation to get the height of the display
-  return 0;
-}
-
-void %s::drawText(int x, int y, std::string_view text, Color fg, Color bg) {
-  // Implementation to draw text on the display
-}
-
-void %s::onInputChar(std::function<void(char)> callback) {
-  // Implementation to handle input characters
-}
 // Vim: set expandtab tabstop=2 shiftwidth=2:
