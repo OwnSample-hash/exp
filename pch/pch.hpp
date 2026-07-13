b5bf8aeb95a95cef15a0441c5b0bc0bd95f2b1f8
OwnSample <ownsample@tutanota.com>
2026-07-13 23:11:46 +0200

    feat: pch bo

---
+++ pch/pch.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include <arpa/inet.h>
+#include <filesystem>
+#include <format>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <memory>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <optional>
+#include <regex>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <unordered_map>
+#include <vector>
+
+// Vim: set expandtab tabstop=2 shiftwidth=2:
