3bab1b424ad15157f8ac4b6573ff4e6e55a918c4
OwnSample <ownsample@tutanota.com>
2026-08-15 16:57:19 +0200

    chore: update package lock

--- cpm-package-lock.cmake
+++ cpm-package-lock.cmake
@@ -8,10 +8,38 @@ CPMDeclarePackage(spdlog
   SYSTEM YES
   EXCLUDE_FROM_ALL YES
 )
+# json
+CPMDeclarePackage(json
+  VERSION 3.12.0
+  GITHUB_REPOSITORY nlohmann/json
+  SYSTEM YES
+  EXCLUDE_FROM_ALL YES
+)
+# yaml-cpp (unversioned)
+# CPMDeclarePackage(yaml-cpp
+#  GIT_TAG yaml-cpp-0.9.0
+#  GITHUB_REPOSITORY jbeder/yaml-cpp
+#  SYSTEM YES
+#  EXCLUDE_FROM_ALL YES
+#)
+# Catch2
+CPMDeclarePackage(Catch2
+  VERSION 3.15.3
+  GITHUB_REPOSITORY catchorg/Catch2
+  SYSTEM YES
+  EXCLUDE_FROM_ALL YES
+)
 # lua
 CPMDeclarePackage(lua
   NAME lua
-  VERSION 5.5.0
+  VERSION 5.5.1
   DOWNLOAD_ONLY YES
   GIT_REPOSITORY https://github.com/lua/lua.git
+)
+# cpp-httplib
+CPMDeclarePackage(cpp-httplib
+  VERSION 0.46.1
+  GITHUB_REPOSITORY yhirose/cpp-httplib
+  SYSTEM YES
+  EXCLUDE_FROM_ALL YES
 )
