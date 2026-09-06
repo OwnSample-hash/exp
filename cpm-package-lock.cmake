20192f840a037777bc298a3cfb44092d602afc27
OwnSample <ownsample@tutanota.com>
2026-09-06 15:50:01 +0200

    feat: make spdlog build both static and shared version
    Also updates CPM

--- cpm-package-lock.cmake
+++ cpm-package-lock.cmake
@@ -3,10 +3,13 @@
 
 # spdlog
 CPMDeclarePackage(spdlog
-  VERSION 1.17.0
-  GITHUB_REPOSITORY gabime/spdlog
-  SYSTEM YES
-  EXCLUDE_FROM_ALL YES
+  NAME spdlog
+  URL
+    "https://github.com/gabime/spdlog/archive/refs/tags/v1.17.0.tar.gz"
+    "URL_HASH"
+    "SHA256=d8862955c6d74e5846b3f580b1605d2428b11d97a410d86e2fb13e857cd3a744"
+    "PATCHES"
+    "patches/spdlog.patch"
 )
 # json
 CPMDeclarePackage(json
